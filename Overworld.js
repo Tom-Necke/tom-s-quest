baa717d9d30b4fa6161b63d89c39518dfffcc25f
Tom Necke <tom.necke@dci-student.de>
2022-04-08 23:26:08 +0200

    break

---
+++ Overworld.js
@@ -0,0 +1,20 @@
+class Overworld {
+    constructor(config) {
+        this.element = config.element;
+        this.canvas = this.element.querySelector(".game-canvas");
+        this.ctx = this.canvas.getContext("2d");
+    }
+
+    init() {
+        const image = new Image();
+        image.onload = () => {
+            //this.ctx.drawImage()
+        };
+
+    }
+
+}
+
+
+
+
