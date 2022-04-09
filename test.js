ce4b5f2f43435e65101e379ce5e6e3eeb1fab3d4
Tom Necke <tom.necke@dci-student.de>
2022-04-09 15:35:31 +0200

    add character theorie

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 class Game {
-    constructor()
+    constructor(name, level) {
+        this.name = "Tom"
+        this.level = 100
+    }
 }
+
+let character1 = new Game("Koji", 50);
+
+
+
+console.log(character1);
