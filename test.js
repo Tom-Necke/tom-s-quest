fb28bc4c1420387b717ec58c87c79391a426c274
Tom Necke <tom.necke@dci-student.de>
2022-04-09 15:30:24 +0200

    object.test

---
+++ test.js
@@ -0,0 +1,17 @@
+let gameObject = {
+    name: "Tom",
+    age: 27,
+    job: "webdev"
+}
+console.log(gameObject);
+
+console.log(gameObject.age);
+
+
+
+class Game_Objects {
+    constructor(name, age) {
+        
+    }
+}
+let 
