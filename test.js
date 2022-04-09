cd61fa3dbbaf38b8ae9e65c4330891f95ccd1971
Tom Necke <tom.necke@dci-student.de>
2022-04-09 15:42:18 +0200

    update.class.test

--- test.js
+++ test.js
@@ -12,3 +12,20 @@ let character2 = new Game("Riki", 23);
 
 console.log(character1);
 console.log(character2);
+
+
+
+
+
+class Assets {
+    constructor(style, tree) {
+        this.style = style;
+        this.tree = tree;
+    }
+}
+
+let assets1 = new Assets("right", "tree1");
+let assets2 = new Assets("example", 40);
+
+console.log(assets1);
+console.log(assets2);
